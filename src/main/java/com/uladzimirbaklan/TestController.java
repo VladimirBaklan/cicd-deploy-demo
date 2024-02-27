a229a50e91a2a452db2adbee0ea6cddac6898fad
VladimirBaklan <uladzimirbaklan@gmail.com>
2024-02-28 00:11:12 +0100

    drop proxy

--- src/main/java/com/uladzimirbaklan/TestController.java
+++ src/main/java/com/uladzimirbaklan/TestController.java
@@ -1,38 +1,17 @@
 package com.uladzimirbaklan;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.UUID;
 
 @RestController
 public class TestController {
-    private final HttpClient httpClient = HttpClient.newBuilder().build();
-
     @GetMapping("/test")
     public ResponseEntity<TestResponse> getTestResponse() {
         return ResponseEntity.ok(new TestResponse(UUID.randomUUID().toString(), "version_2"));
     }
-
-    @PostMapping("/proxy")
-    public ResponseEntity<String> getProxyRequest(@RequestBody ProxyRequestBody requestBody)
-        throws URISyntaxException, IOException, InterruptedException {
-        final var request = HttpRequest
-            .newBuilder()
-            .uri(new URI(requestBody.uri))
-            .build();
-
-        final var response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-        return ResponseEntity.ok(response.body());
-    }
-
-    public record ProxyRequestBody(String uri) {}
 
     public record TestResponse(String id, String version) {
     }
